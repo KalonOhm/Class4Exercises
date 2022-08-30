b308a2d6f65dd92d96eedb0f738e0046ec786f8e
Kalon Ohmstede <kalonohmstede@gmail.com>
2022-08-29 20:26:15 -0500

    updates

--- Exercise2.js
+++ Exercise2.js
@@ -1,13 +1,18 @@
-
-let countOfTrue = 0;
 function countTrue(bools) {
-    
-    for (let i=0; i <= bools.length; i++){
-        if (bools[i] === true){
-            countOfTrue+=1;
-        }
-        return countOfTrue;
+  let trueArray = [];
+  for (let i = 0; i <= bools.length; i++) {
+    if (bools[i] === true) {
+      trueArray.push(true);
     }
-
-console.log(countOfTrue);
+  }
+  console.log(trueArray.length);
+  return trueArray.length;
 }
+
+countTrue([true, false, false, true, false]); // ➞    2
+
+countTrue([false, false, false, false]); //➞    0
+
+countTrue([]); //➞    0
+
+countTrue([true, true, false, true, false, true, false, false, false, true]); //➞ 5
